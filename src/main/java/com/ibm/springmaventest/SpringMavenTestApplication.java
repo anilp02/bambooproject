7182245a198c439c7d63596f7b5de021f874a750
anilp02 <anilkumarp31@gmail.com>
2018-06-07 06:31:29 -0400

    inital

---
+++ src/main/java/com/ibm/springmaventest/SpringMavenTestApplication.java
@@ -0,0 +1,12 @@
+package com.ibm.springmaventest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMavenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMavenTestApplication.class, args);
+	}
+}
